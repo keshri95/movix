e256291e6915b6bc61eb1376ae35cbb7e6f10f37
Ashish Keshri <keshri.ashish1@gmail.com>
2023-02-18 19:43:44 +0530

    home-page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { useDispatch, useSelector } from "react-redux";
-import { getApiConfiguration } from "./store/homeSlice";
+import { getApiConfiguration, getGenres } from "./store/homeSlice";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Details from "./pages/details/Details";
@@ -20,8 +20,10 @@ function App() {
   )
 
 
+
   useEffect(() => {
     fetchApiConfig();
+    genresCall();
   }, []);
 
   const fetchApiConfig = () => {
@@ -40,6 +42,27 @@ function App() {
 
     });
   };
+
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`));
+    });
+
+    const data = await Promise.all(promises);
+    data.map(({ genres }) => {
+      return genres.map((item) => (allGenres[item.id] = item))
+    })
+
+    dispatch(getGenres(allGenres));
+
+  }
+
+
   return (
 
     <BrowserRouter>
